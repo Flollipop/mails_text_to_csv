e94256c05d4355132bff3509584d0038b4af5be0
russo <florian.carusso@gmail.com>
2020-05-09 16:40:59 +0200

    :recycle: Apply eslint & prettier config to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Converter from "./Converter";
+import Converter from './Converter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          <Converter/>
+    <div className='App'>
+      <header className='App-header'>
+        <Converter />
       </header>
     </div>
   );
--- src/Converter.js
+++ src/Converter.js
@@ -1,42 +1,38 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 const labelTextArea = 'emails :';
-const containerStyle = { display: 'flex', width: '80%', height: '600px'};
+const containerStyle = { display: 'flex', width: '80%', height: '600px' };
 
 const style = {
-    alignItems: 'flex-start',
-    flexGrow: '1',
-    marginTop: '5px',
-    marginLeft: '25px',
-    marginRight: '25px',
-    marginBottom: '25px'
+  alignItems: 'flex-start',
+  flexGrow: '1',
+  marginTop: '5px',
+  marginLeft: '25px',
+  marginRight: '25px',
+  marginBottom: '25px',
 };
 
 class Converter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            textAreaValue: ""
-        };
-        this.handleChange = this.handleChange.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      textAreaValue: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    handleChange(event) {
-        this.setState({ textAreaValue: event.target.value });
-    }
+  handleChange(event) {
+    this.setState({ textAreaValue: event.target.value });
+  }
 
-    render() {
-        return (
-            <div style={containerStyle}>
-                <label>{labelTextArea}</label>
-                <textarea
-                    style={style}
-                    value={this.state.textAreaValue}
-                    onChange={this.handleChange}
-                />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div style={containerStyle}>
+        <label>{labelTextArea}</label>
+        <textarea style={style} value={this.state.textAreaValue} onChange={this.handleChange} />
+      </div>
+    );
+  }
 }
 
 export default Converter;
