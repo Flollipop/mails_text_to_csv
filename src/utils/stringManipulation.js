857888f049af633f7378fdf4d771a9b286edb3ed
russo <florian.carusso@gmail.com>
2020-05-09 16:47:25 +0200

    :sparkles: Add mail parser in string manipulation utils

---
+++ src/utils/stringManipulation.js
@@ -0,0 +1,26 @@
+import * as linkify from 'linkify-it';
+
+// as we do not need http parsing with linkify we override the default schema to avoid useless match
+const httpOverride = {
+  'http:': {
+    validate: function () {
+      return 0;
+    },
+  },
+};
+
+const parser = linkify(httpOverride, {
+  fuzzyLink: false,
+  fuzzyEmail: true,
+  fuzzyIP: false,
+});
+
+export function parseMails(str = '') {
+  const rawMails = parser.match(str);
+
+  if (rawMails && rawMails.length > 0) {
+    return rawMails.map(x => x.raw);
+  }
+
+  return [];
+}
