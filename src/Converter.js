b5ef5a36353ccdaa3382a60c8ed734e522d56096
russo <florian.carusso@gmail.com>
2020-05-09 12:33:55 +0200

    :construction: Create multi line input component
    Empty line input with no logic

---
+++ src/Converter.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+const labelTextArea = 'emails :';
+const containerStyle = { display: 'flex', width: '80%', height: '600px'};
+
+const style = {
+    alignItems: 'flex-start',
+    flexGrow: '1',
+    marginTop: '5px',
+    marginLeft: '25px',
+    marginRight: '25px',
+    marginBottom: '25px'
+};
+
+class Converter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            textAreaValue: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ textAreaValue: event.target.value });
+    }
+
+    render() {
+        return (
+            <div style={containerStyle}>
+                <label>{labelTextArea}</label>
+                <textarea
+                    style={style}
+                    value={this.state.textAreaValue}
+                    onChange={this.handleChange}
+                />
+            </div>
+        );
+    }
+}
+
+export default Converter;
