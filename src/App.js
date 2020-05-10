3f09f9d27157748ff1b70cec47e32f7e6ca17c5c
russo <florian.carusso@gmail.com>
2020-05-10 10:56:52 +0200

    :speech_balloon: Add page title in header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 import Converter from './Converter';
 
+const headerLabel = 'Email address parser to csv';
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <header className='App-header'>{headerLabel}</header>
       <Converter />
     </div>
   );
