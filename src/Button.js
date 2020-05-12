9620a6c41fadb63915632c19755c2917131aa8b1
russo <florian.carusso@gmail.com>
2020-05-12 09:35:31 +0200

    :recycle: Rm unused inline style in button

--- src/Button.js
+++ src/Button.js
@@ -1,14 +1,8 @@
 import * as PropTypes from 'prop-types';
 import React from 'react';
 
-const buttonStyle = {};
-
 export function Button(props) {
-  return (
-    <button style={buttonStyle} onClick={props.onClick}>
-      convert
-    </button>
-  );
+  return <button onClick={props.onClick}>convert</button>;
 }
 
 Button.propTypes = { onClick: PropTypes.func };
