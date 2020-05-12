c65ee085dff0db2405576e2d0afc5af0ab93d876
russo <florian.carusso@gmail.com>
2020-05-12 17:16:40 +0200

    :recycle: Replace jest expect with chai

--- src/utils/stringManipulation.test.js
+++ src/utils/stringManipulation.test.js
@@ -1,7 +1,12 @@
 import { parseMails } from './stringManipulation';
+import chai from 'chai';
+import dirtyChai from 'dirty-chai';
+import createChaiJestDiff from 'chai-jest-diff';
+
+chai.use(dirtyChai).use(createChaiJestDiff());
 
 test('parseMail - no email', () => {
   const noMails = '1234 test.test.com qwerty@qwerty https://google.fr http://google.fr';
   const res = parseMails(noMails);
-  expect(res.length).toBe(0);
+  chai.expect(res.length).to.be.equal(0);
 });
