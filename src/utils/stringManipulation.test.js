224df4f69a5a961663d64cc034ab01cbb3ae2cb1
russo <florian.carusso@gmail.com>
2020-05-12 18:30:55 +0200

    :white_check_mark: Add mail compare in test

--- src/utils/stringManipulation.test.js
+++ src/utils/stringManipulation.test.js
@@ -22,4 +22,16 @@ test('parseMail - 1 email', () => {
   chai.expect(res[0].mail).to.be.equal(mail1);
 });
 
-const mails = ['email1@email.fr'];
+const mailTemplate = n => `email${n}@email.com`;
+// generate array of emails using mailTemplate like for n = 3 the returned array equals ['email0@email.com', 'email1@email.com', 'email2@email.com']
+const mailsArrayGenerator = n => Array.apply(null, { length: n }).map(Function.call, mailTemplate);
+const mailsArray = mailsArrayGenerator(1000);
+
+// create a big string with 1000 mails formatted like <email0@email.com>;<email1@email.com>;<email2@email.com>; ... ;<email997@email.com>;<email998@email.com>;<email999@email.com>
+const lotOfFormattedMails = `<${mailsArray.join('>;<')}>`;
+
+test('parseMail - 1000 emails', () => {
+  const res = parseMails(lotOfFormattedMails);
+  chai.expect(res.length).to.be.equal(1000);
+  chai.expect(res.map(x => x.mail)).to.deep.equal(mailsArray);
+});
