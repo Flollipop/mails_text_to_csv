d7ceb5b4066589cf086f841e2cac1e4e4bd2064a
russo <florian.carusso@gmail.com>
2020-05-12 09:37:41 +0200

    :recycle: Use obj destructuring on Button props

--- src/Button.js
+++ src/Button.js
@@ -1,8 +1,8 @@
 import * as PropTypes from 'prop-types';
 import React from 'react';
 
-export function Button(props) {
-  return <button onClick={props.onClick}>convert</button>;
+export function Button({ onClick }) {
+  return <button onClick={onClick}>convert</button>;
 }
 
 Button.propTypes = { onClick: PropTypes.func };
