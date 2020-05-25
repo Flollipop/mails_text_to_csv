c9a43a9c3cebb70327462ba3322c1dd176321342
russo <florian.carusso@gmail.com>
2020-05-25 18:22:23 +0200

    :white_check_mark: Add button OnClick test

--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Button, buttonTestId } from './Button';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 test('render button - smoke test', () => {
   const div = document.createElement('div');
@@ -13,4 +13,14 @@ test('renders button test label', () => {
   const labelToTest = 'button label';
   const { getByTestId } = render(<Button label={labelToTest} />);
   expect(getByTestId(buttonTestId)).toHaveTextContent(labelToTest);
+});
+
+test('renders button test label', () => {
+  const handleClose = jest.fn();
+  const labelToTest = 'button label';
+  const { getByTestId } = render(<Button label={labelToTest} onClick={handleClose} />);
+
+  expect(getByTestId(buttonTestId)).toBeTruthy();
+  fireEvent.click(screen.getByTestId(buttonTestId));
+  expect(handleClose).toHaveBeenCalledTimes(1);
 });
