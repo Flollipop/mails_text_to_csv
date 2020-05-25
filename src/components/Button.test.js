4be6400a838d428f2d8358a65266b11badd3824a
russo <florian.carusso@gmail.com>
2020-05-25 17:46:12 +0200

    :white_check_mark: Add button label test

--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button } from './Button';
+import { Button, buttonTestId } from './Button';
+import { render } from '@testing-library/react';
 
 test('render button - smoke test', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Button />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('renders button test label', () => {
+  const labelToTest = 'button label';
+  const { getByTestId } = render(<Button label={labelToTest} />);
+  expect(getByTestId(buttonTestId)).toHaveTextContent(labelToTest);
 });
