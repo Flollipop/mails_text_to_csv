20a1f45ec94400684d808aecf872c7c237b75cad
russo <florian.carusso@gmail.com>
2020-05-25 15:39:03 +0200

    :recycle: Mv header in components directory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Converter from './Converter';
-import { Header } from './Header';
+import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 
 function App() {
