3f6b060b6a3591d3e9de6811a4b8ae33faf27c93
russo <florian.carusso@gmail.com>
2020-05-27 17:12:05 +0200

    :white_check_mark: Add snapshot test for header

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Header } from './Header';
+import renderer from 'react-test-renderer';
 
 test('render header - smoke test', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Header />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('Header snapshot', () => {
+  const tree = renderer.create(<Header />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
