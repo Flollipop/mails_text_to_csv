3f1c31a517e13fd0c9393a8969a25e10b30c41fe
russo <florian.carusso@gmail.com>
2020-05-27 19:01:04 +0200

    :white_check_mark: Add user update text area test

--- src/components/Converter.test.js
+++ src/components/Converter.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Converter from './Converter';
+import { render, screen, fireEvent, cleanup } from '@testing-library/react';
+
+afterEach(cleanup);
 
 test('render converter - smoke test', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Converter />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('user enter input in textArea', () => {
+  render(<Converter />);
+  const textArea = screen.getByTestId('textArea');
+  const userInput = 'test to test test@test.fr';
+  fireEvent.change(textArea, { target: { value: userInput } });
+  expect(textArea.value).toEqual(userInput);
 });
