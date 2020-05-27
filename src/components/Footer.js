e04397fb42bbfb944d457ade831d6d2095b399d9
russo <florian.carusso@gmail.com>
2020-05-27 17:53:56 +0200

    :construction: Add github logo to footer repo link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { ReactComponent as GitHubLogo } from '../static/github-logo.svg';
 const repoLink = 'https://github.com/Flollipop/mails_text_to_csv';
 const footerLabel = 'See the code';
 
@@ -7,6 +7,7 @@ export function Footer() {
   return (
     <footer className={'App-footer'}>
       <a className={'App-link'} href={repoLink} target='_blank' rel='noopener noreferrer'>
+        <GitHubLogo className={'Github-logo'} />
         {footerLabel}
       </a>
     </footer>
