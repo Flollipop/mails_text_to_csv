0003c195bfe84fa6bcee7b9cb63bd15b59cef990
russo <florian.carusso@gmail.com>
2020-05-27 17:29:55 +0200

    :white_check_mark: Add snapshot test for footer

--- src/components/Footer.test.js
+++ src/components/Footer.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Footer } from './Footer';
 import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 
 test('render footer - smoke test', () => {
   const div = document.createElement('div');
@@ -15,4 +16,9 @@ test('github link', () => {
 
   expect(linkElement.href).toBe('https://github.com/Flollipop/mails_text_to_csv');
   expect(linkElement).toBeInTheDocument();
+});
+
+test('Footer snapshot', () => {
+  const tree = renderer.create(<Footer />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
