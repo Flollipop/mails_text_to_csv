995950d3dd7fcc3ef750887465309d76135d1934
russo <florian.carusso@gmail.com>
2020-05-27 17:27:53 +0200

    :white_check_mark: Add repo link test

--- src/components/Footer.test.js
+++ src/components/Footer.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Footer } from './Footer';
+import { render } from '@testing-library/react';
 
 test('render footer - smoke test', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Footer />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('github link', () => {
+  const { getByText } = render(<Footer />);
+  const linkElement = getByText('See the code');
+
+  expect(linkElement.href).toBe('https://github.com/Flollipop/mails_text_to_csv');
+  expect(linkElement).toBeInTheDocument();
 });
